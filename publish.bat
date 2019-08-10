1b489bbd0e2ce6fcc768fecf173b078a0fffc176
Ricardo Costeira <rcosteira79@gmail.com>
2019-08-10 14:19:02 +0100

    Added publishing script to automatically publish the website. Only works for windows though.

---
+++ publish.bat
@@ -0,0 +1,30 @@
+@echo off
+REM Very simple script to automate deployment to the actual website repo (rcosteira79.github.io)
+
+set /p commitMessage=Commit Description: 
+
+if exist "./_site" @RD /S /Q "./_site"
+
+echo Building with Jekyll...
+set JEKYLL_ENV=production
+call bundle exec jekyll build
+
+echo Copying output to website repo...
+
+REM exclude README.md and the pictures with "original" text in name.
+REM I want the README.md that's already on the site repo, and the pictures are not necessary
+robocopy "./_site" "../rcosteira79.github.io" /e /njh /njs /ndl /nc /ns /np /nfl /xf "README.md" "*-original.*" "publish.bat" 
+
+echo Done. Walking into the website's directory...
+
+cd "../rcosteira79.github.io"
+
+echo Commiting!
+
+git add .
+
+git commit -m "%commitMessage%"
+
+git push
+
+echo All done. Go and see the website on https://ricardocosteira.com :)
