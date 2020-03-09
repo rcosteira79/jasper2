2b25d7316f6183ada7115f257e874304833ae3bc
Ricardo Costeira <ricardo.costeira@mindera.com>
2020-03-09 12:12:27 +0000

    Uncommented git commands in publish.sh

--- publish.sh
+++ publish.sh
@@ -17,7 +17,7 @@ echo Copying output to website repo...
 
 # exclude README.md and the pictures with "original" text in name.
 # I want the README.md that's already on the site repo, and the original pictures are not necessary
-rsync -avr --exclude=".DS_Store" --exclude="README.md" --exclude="assets/images/*-original.*" --exclude="publish.bat" --exclude="publish.sh" "./_site/" "../rcosteira79.github.io"
+rsync -avr --exclude="*DS_Store" --exclude="README.md" --exclude="assets/images/*-original.*" --exclude="publish.bat" --exclude="publish.sh" "./_site/" "../rcosteira79.github.io"
 
 echo Done. Walking into the website\'s directory...
 
@@ -25,10 +25,10 @@ cd "../rcosteira79.github.io"
 
 echo Commiting!
 
-#git add .
+git add .
 
-#git commit -m "%commitMessage%"
+git commit -m "%commitMessage%"
 
-#git push
+git push
 
 echo All done. Go and see the website on https://ricardocosteira.com :\)
